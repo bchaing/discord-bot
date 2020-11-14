4838f07afc63c2a1337bb19baf84dd901c7f0132
Brandon Chaing <bchaing@ucsd.edu>
2020-11-13 23:57:30 -0800

    Added comments to bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -8,9 +8,14 @@ module.exports = {
     usage: '<user>',
     cooldown: 10,
 	execute(message, args) {
+        // creates variables for taggedMember and bonkChannel
         const taggedMember = (message.mentions.members.first());
         const bonkChannel = message.guild.channels.cache.get('750271593401417779');
+        
+        // send bonk message
         message.channel.send(`GO TO HORNY JAIL ${taggedMember.user}`,{files: ["https://media1.tenor.com/images/6493bee2be7ae168a5ef7a68cf751868/tenor.gif?itemid=17298755"]});
+        
+        // move user to bonk channel
         taggedMember.edit({channel:bonkChannel}).catch(err => console.log(err));
 	},
 };
