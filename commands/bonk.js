885b7cf43633f9293bea518fdaa583fa464c8bbb
Brandon Chaing <bchaing@ucsd.edu>
2020-11-13 23:34:45 -0800

    Updated bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -6,10 +6,12 @@ module.exports = {
     guildOnly: true,
     args: true,
     usage: '<user>',
+    cooldown: 10,
 	execute(message, args) {
         const taggedUser = message.mentions.users.first();
+        const taggedMember = (message.mentions.members.first());
+        const bonkChannel = message.guild.channels.cache.get('750271593401417779');
         message.channel.send(`GO TO HORNY JAIL ${taggedUser}`,{files: ["https://media1.tenor.com/images/6493bee2be7ae168a5ef7a68cf751868/tenor.gif?itemid=17298755"]});
-        //const taggedUser = (message.mentions.members.first());
-        //taggedUser.edit({channel:750271593401417779}).catch(err => console.log(err));
+        taggedMember.edit({channel:bonkChannel}).catch(err => console.log(err));
 	},
 };
