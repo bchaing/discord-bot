b1efae7e994005a441f33242512e47a5fc85d14b
Brandon Chaing <bchaing@ucsd.edu>
2020-11-14 12:21:39 -0800

    Updated bonk.js with error checking

--- commands/bonk.js
+++ commands/bonk.js
@@ -4,15 +4,30 @@ module.exports = {
     guildOnly: true,
     args: true,
     usage: '<user>',
-    cooldown: 10,
+    cooldown: 0,
 	execute(message, args) {
         // creates variables for taggedMember and bonkChannel
-        const taggedMember = (message.mentions.members.first());
+        let taggedMember = (message.mentions.members.first());
         const bonkChannel = message.guild.channels.cache.get('750271593401417779');
+
+        // check if taggedMember is valid
+        if (!taggedMember) {
+            message.channel.send(`${args[0]} is not a valid user!`);
+            taggedMember = message.member;
+
+            if (!taggedMember.voice.connection) {
+                return;
+            }
+        }
         
+        // check if taggedMember is in a voice channel
+        if (!taggedMember.voice.connection) {
+            return message.channel.send(`${taggedMember.user} is not in a valid voice channel!`);
+        }
+
         // send bonk message
         message.channel.send(`GO TO HORNY JAIL ${taggedMember.user}`,{files: ["https://media1.tenor.com/images/6493bee2be7ae168a5ef7a68cf751868/tenor.gif?itemid=17298755"]});
-        
+
         // move user to bonk channel
         taggedMember.edit({channel:bonkChannel}).catch(err => console.log(err));
 	},
