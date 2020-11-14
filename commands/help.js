b558998f8a20dbc698f1089b596aac642ea8fb25
Brandon Chaing <bchaing@ucsd.edu>
2020-11-14 12:26:16 -0800

    Changed help.js to post in server rather than DM's

--- commands/help.js
+++ commands/help.js
@@ -7,40 +7,29 @@ module.exports = {
 	usage: '[command name]',
 	cooldown: 0,
 	execute(message, args) {
-		const data = [];
         const { commands } = message.client;
 
         if (!args.length) {
-            data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+            message.channel.send('Here\'s a list of all my commands:');
+            message.channel.send(commands.map(command => command.name).join(', '));
+            message.channel.send(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-            return message.author.send(data, { split: true })
-                .then(() => {
-                    if (message.channel.type === 'dm') return;
-                    message.reply('I\'ve sent you a DM with all my commands!');
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-                });
+            return;
         }
 
         const name = args[0].toLowerCase();
         const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
         if (!command) {
-            return message.reply('that\'s not a valid command!');
+            return message.channel.send(`${command} is not a valid command!`);
         }
 
-        data.push(`**Name:** ${command.name}`);
+        message.channel.send(`**Name:** ${command.name}`);
 
-        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-        if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        if (command.aliases) message.channel.send(`**Aliases:** ${command.aliases.join(', ')}`);
+        if (command.description) message.channel.send(`**Description:** ${command.description}`);
+        if (command.usage) message.channel.send(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
-        data.push(`**Cooldown:** ${command.cooldown || 0} second(s)`);
-
-        message.channel.send(data, { split: true });
+        message.channel.send(`**Cooldown:** ${command.cooldown || 0} second(s)`);
 	},
 };
