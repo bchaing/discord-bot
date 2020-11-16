b9108fe4a9c37d7e7f2ef1a90992e4e9e839cf49
Brandon Chaing <bchaing@ucsd.edu>
2020-11-16 10:27:26 -0800

    Added comments to reload.js

--- commands/kill.js
+++ commands/kill.js
@@ -6,13 +6,16 @@ module.exports = {
     usage: '',
     cooldown: 0,
 	execute(message, args) {
+        // checks if the user is an admin on the server
         if(!message.member.hasPermission('ADMINISTRATOR')) {
             message.channel.send('You must have permission to kill the bot!');
             return;
         }
         
-        message.channel.send(`Bonking ${message.client.user}, shutting down...`).then(() => {
+        // sends shutdown message and destroys client
+        message.channel.send('Shutting down...').then(() => {
             message.client.destroy();
         });
+
 	},
 };
