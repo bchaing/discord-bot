6888ab035eb298252524a38817e05e1ef76fd7cb
Eldrin Pile <justeldrin@gmail.com>
2020-11-15 22:44:31 -0800

    FOR BRANDON

--- commands/lolstats.js
+++ commands/lolstats.js
@@ -22,17 +22,21 @@ module.exports = {
             // completes URL from input
             const userName = args[0];
 
+            // presses okay button from cookies
 
 
             // create a new browser page
             const page = await browser.newPage();
+            const form = await page.$('body > div > div.message.type-bottom.no-border > div.message-component.message-row.message-row > button.message-component.message-button.no-children.accept-button');
+            await form.evaluate( form => form.click() );
+
             try {
                 // navagate to league of legends live page
-                await page.goto(`https://u.gg/lol/profile/na1/${ userName }/live-game`, { waitUntil: 'networkidle0' });
+                await page.goto(`https://u.gg/lol/profile/na1/${userName}/live-game`, { waitUntil: 'networkidle0' });
 
                 // wait for page to load and screen shot the stats element
-                await page.waitForSelector('#content > div.summoner-profile-container.content-side-padding > div.summoner-profile_content-container > div > div.live-game-container');          // wait for the selector to load
-                const element = await page.$('#content > div.summoner-profile-container.content-side-padding > div.summoner-profile_content-container > div > div.live-game-container');        // declare a variable with an ElementHandle
+                await page.waitForSelector('#content > div.summoner-profile-container.content-side-padding > div.summoner-profile_content-container > div > div.live-game-container > div');          // wait for the selector to load
+                const element = await page.$('#content > div.summoner-profile-container.content-side-padding > div.summoner-profile_content-container > div > div.live-game-container > div');        // declare a variable with an ElementHandle
                 await element.screenshot({ path: 'images/lolstats.png' }); // take screenshot element in puppeteer
             } catch (error) {
                 message.channel.send("An error occurred retrieving your lolstats page");
