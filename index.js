2be788a7ce0f360799c625cdf6089020a1602401
Brandon Chaing <bchaing@ucsd.edu>
2020-11-20 22:00:16 -0800

    Updated index.js to handle role change events

--- index.js
+++ index.js
@@ -88,5 +88,14 @@ client.on('message', message => {
     }
 });
 
+client.on('guildMemberUpdate', (oldMember, newMember) => {
+	// If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
+	const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+	if (removedRoles.size > 0) console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
+	// If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
+	const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+	if (addedRoles.size > 0) console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
+});
+
 // login to Discord with your app's token
 client.login(token);
