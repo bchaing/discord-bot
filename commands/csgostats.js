34f936dc5031bef9649f6207145bc72caa358ffe
Brandon Chaing <bchaing@ucsd.edu>
2020-11-22 16:45:52 -0800

    Updated args for csgostats.js

--- commands/csgostats.js
+++ commands/csgostats.js
@@ -43,10 +43,10 @@ module.exports = {
 
             // creating browser
             const browser = await puppeteer.launch({
-                product: 'chrome', 
-                executablePath: '/usr/bin/chromium',
+                // product: 'chrome', 
+                // executablePath: '/usr/bin/chromium',
                 ignoreHTTPSErrors: true,
-                args: ['--window-size=1920,1080'],
+                args: ['--window-size=1920,1080', '--no-sandbox'],
             });
 
             // create a new browser page
