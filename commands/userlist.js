4bab6b703d68786bdf0322421162126b1bdfe43e
Brandon Chaing <bchaing@ucsd.edu>
2020-11-25 23:12:39 -0800

    Update value of map

--- commands/userlist.js
+++ commands/userlist.js
@@ -16,7 +16,7 @@ module.exports = {
             }); */
 
             for (const [key, value] of members.entries()) {
-                members.set(value, key.roles.cache);
+                members.set(value, value.roles.cache);
                 members.delete(key);
             }
             
