c31235504457ff4a05add6e90be496727ec2c36b
Brandon Chaing <bchaing@ucsd.edu>
2020-11-25 19:40:36 -0800

    Test update console.log

--- commands/userlist.js
+++ commands/userlist.js
@@ -10,7 +10,7 @@ module.exports = {
 
             members.map(key => {
                 members[key] = key.nickname;
-                console.log(`${key} : ${members[key]}`);
+                console.log(`${key.user.username} : ${members[key]}`);
             });
           })();
 	},
