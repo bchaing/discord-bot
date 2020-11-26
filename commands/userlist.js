2ba381b139eee0e089bb19f77bfb85b4836ff3d8
Brandon Chaing <bchaing@ucsd.edu>
2020-11-25 21:51:32 -0800

    Remove catch

--- commands/userlist.js
+++ commands/userlist.js
@@ -11,7 +11,7 @@ module.exports = {
             const members = await message.guild.members.fetch();
 
             await members.map(key => {members[key] = key.roles.cache;});
-            fs.writeFileSync('modules/rolepersist.json', JSON.stringify(members), 'utf-8').catch(err => console.log(err)); 
+            fs.writeFileSync('modules/rolepersist.json', JSON.stringify(members), 'utf-8'); 
           })();
 	},
 };
