b3bb466787374133f03e4b0c3a6fdb615362754e
Brandon Chaing <bchaing@ucsd.edu>
2020-12-06 14:39:25 -0800

    Updated dependencies

---
+++ commands/join.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: 'join',
+    description: 'Joins the calling member\'s voice channel.',
+    guildOnly: true,
+    args: false,
+    usage: '',
+	execute(message, args) {
+        (async () => {
+            // Only try to join the sender's voice channel if they are in one themselves
+            if (message.member.voice.channel) {
+                const connection = await message.member.voice.channel.join();
+            } else {
+                message.reply('You need to join a voice channel first!');
+            }
+        })();
+	},
+};
