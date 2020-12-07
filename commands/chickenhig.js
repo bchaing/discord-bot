55bd2b6f27f51365c210f0bfd653a1f45f86dffa
Brandon Chaing <bchaing@ucsd.edu>
2020-12-06 18:05:36 -0800

    Add chikenhig gif message

---
+++ commands/chickenhig.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name: 'chickenhig',
+    description: 'Plays chikenhig insults.',
+    guildOnly: true,
+    args: false,
+    usage: '',
+	execute(message) {
+        (async () => {
+            // Only try to join the sender's voice channel if they are in one themselves
+            let connection;
+            if (message.member.voice.channel) {
+                connection = await message.member.voice.channel.join();
+            } else {
+                message.reply('You need to join a voice channel first!');
+            }
+
+            message.channel.send('https://gfycat.com/allgrimdrafthorse');
+            const dispatcher = connection.play('audio.mp4', { volume: 0.30 });
+            
+            dispatcher.on('finish', () => {
+                connection.disconnect();
+            });
+        })();
+	},
+};
