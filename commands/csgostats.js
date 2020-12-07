8e731bf1621cb3c8d3146690ba80cafd5c4046a5
Brandon Chaing <bchaing@ucsd.edu>
2020-12-07 01:30:39 -0800

    Testing updating image

--- commands/csgostats.js
+++ commands/csgostats.js
@@ -102,18 +102,10 @@ module.exports = {
 
             // send image to the chat
             const file = await new MessageAttachment('images/csgostats.png');
+            csgoEmbed.description = '';
             csgoEmbed.image = { url: 'attachment://csgostats.png' };
 
-            const exampleEmbed = {
-                title: 'Some title',
-                image: {
-                    url: 'attachment://csgostats.png',
-                },
-            };
-            
-            message.channel.send({ files: [file], embed: exampleEmbed });
-
-            await msg.edit({ files: [file], embed: csgoEmbed });
+            msg.edit({ files: [file], embed: csgoEmbed });
                         
             // delete image after being sent
             const fs = require('fs');
