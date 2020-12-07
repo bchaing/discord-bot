cf4aad5e6460a9a3e16a15a9c03c06b80505f02c
Brandon Chaing <bchaing@ucsd.edu>
2020-12-06 18:08:22 -0800

    Update join to do nothing else but join

--- commands/join.js
+++ commands/join.js
@@ -7,18 +7,11 @@ module.exports = {
 	execute(message) {
         (async () => {
             // Only try to join the sender's voice channel if they are in one themselves
-            let connection;
             if (message.member.voice.channel) {
-                connection = await message.member.voice.channel.join();
+                message.member.voice.channel.join();
             } else {
                 message.reply('You need to join a voice channel first!');
             }
-
-            const dispatcher = connection.play('audio.mp4', { volume: 0.30 });
-            
-            dispatcher.on('finish', () => {
-                connection.disconnect();
-            });
         })();
 	},
 };
