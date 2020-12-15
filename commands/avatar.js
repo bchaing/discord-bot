f96b58ea5b96585a7d6fb0a44eaa8781ce304da9
Brandon Chaing <bchaing@ucsd.edu>
2020-12-15 13:09:57 -0800

    Add avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,29 @@
+module.exports = {
+	name: 'avatar',
+	description: 'Retrieves the profile picture of a user.',
+	aliases: ['av', 'icon', 'pfp'],
+	usage: '<user>, <user>, ...',
+	cooldown: 0,
+	execute(message, args) {
+        let taggedMember = message.mentions.members;
+
+        if (taggedMember.size) {
+            taggedMember.each((member) => { 
+                message.channel.send({ files: [member.user.avatarURL()] });
+            });
+        } else {
+            args = args.toString().split(',,').map(elem => elem.replace(/,/g, ' ').trim());
+            args.forEach(arg => {
+                taggedMember = message.guild.members.cache.find(m => m.user.username.toLowerCase() === arg.toLowerCase());
+
+                if (taggedMember === undefined) {
+                    taggedMember = message.guild.members.cache.find(m => String(m.nickname).toLowerCase() === arg.toLowerCase());
+                }
+
+                if (taggedMember != undefined) message.channel.send({ files: [taggedMember.user.avatarURL()] });
+            });
+        }
+
+        if (taggedMember === undefined) message.channel.send('Cannot find user!');
+	},
+};
