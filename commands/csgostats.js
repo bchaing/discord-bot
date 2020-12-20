9787a896300bbb9198af730ecd581e1248fb9e65
Brandon Chaing <bchaing@ucsd.edu>
2020-12-20 15:22:01 -0800

    Update comments for raspberry pi options

--- commands/csgostats.js
+++ commands/csgostats.js
@@ -60,7 +60,8 @@ module.exports = {
         csgoEmbed.description = '\`[00000000------------]\` Starting chromium browser';
         msg.edit(csgoEmbed);
         const browser = await puppeteer.launch({
-                // options for raspberry pi hosting
+                /* options for raspberry pi hosting
+                 * uncomment the two lines below and delete the headless option */
                 // product: 'chrome'
                 // executablePath: 'chromium-browser',
                 headless: true,
