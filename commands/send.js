d19dca4955e1ff21fe68843d8bad220ddf4ce775
Brandon Chaing <bchaing@ucsd.edu>
2020-12-26 02:37:57 -0800

    Update comments for clarity

--- commands/send.js
+++ commands/send.js
@@ -7,21 +7,27 @@ module.exports = {
     args: true,
     usage: '<channel> <message>',
 	execute(message) {
+        // check if the caller is the owner of the bot
         if(message.author.id == ownerID) {
             const channel = message.mentions.channels.first();
+            
+            // check if the tagged channel exists
             if (!channel) {
                 message.channel.send('You need to tag a channel!');
                 return;
             }
    
+            // reformats the content of the message and sends it as the bot
             const msg = message.content.replace(`<#${channel.id}> `, '').replace(`b!send `, '');
             if (msg || message.attachments.size) {
                 channel.send(`${msg}`, message.attachments.array()).catch(console.error);
                 console.log(`Sent "${msg}" in #${channel.name}`);
             } 
 
+            // delete the original message
             message.delete();
         } else {
+        // respond to users that are not the bot owner
             message.channel.send("Only the bot owner can use this command!");
         }
 	},
