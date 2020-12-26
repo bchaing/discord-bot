16bbc91edbfeafb24bceb12f374b0c0f71aab978
Brandon Chaing <bchaing@ucsd.edu>
2020-12-26 02:35:46 -0800

    Update comments for clarity

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,8 @@ module.exports = {
 	name: 'ping',
     description: 'Responds to the user and prints the latency of message replies.',
 	execute(message) {
-		message.channel.send('🏓 Pong!').then((newMsg) => {
+        // sends an initial message, measures reply latency and edits message with result
+        message.channel.send('🏓 Pong!').then((newMsg) => {
             const ping = newMsg.createdTimestamp - message.createdTimestamp;
             newMsg.edit(`🏓 Pong! \`${ping}ms\``);
            });
