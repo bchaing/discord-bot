cbd03a930ba3c12b0d35c785ba8c31e71efdbdb7
Brandon Chaing <bchaing@ucsd.edu>
2020-12-25 23:29:11 -0800

    Update comments for clarity

--- commands/chikenhig.js
+++ commands/chikenhig.js
@@ -2,34 +2,37 @@ module.exports = {
 	name: 'chikenhig',
     description: 'Plays chikenhig insults.',
     guildOnly: true,
-	execute(message, args) {
-        (async () => {
-            // Only try to join the sender's voice channel if they are in one themselves
-            let connection;
-            if (args.length === 0) {
-                if (message.member.voice.channel) {
-                    connection = await message.member.voice.channel.join();
-                    console.log(`Joined voice channel: ${message.member.voice.channel.name}`);
-                } else {
-                    message.reply('You need to specify a voice channel to join!');
-                }
+	async execute(message, args) {
+        let connection;
+        
+        if (args.length === 0) {
+        // if no channel is explicitly specified, join author's voice channel if possible
+            if (message.member.voice.channel) {
+                connection = await message.member.voice.channel.join();
+                console.log(`Joined voice channel: ${message.member.voice.channel.name}`);
             } else {
-                const voiceChannel = message.guild.channels.cache.find(vc => vc.name.toLowerCase() === args.join(' ').toLowerCase());
-                
-                if (voiceChannel) {
-                    connection = await voiceChannel.join();
-                    console.log(`Joined voice channel: ${voiceChannel.name}`);
-                } else {
-                    message.reply('You need to specify a valid voice channel to join!');
-                }
+                message.reply('You need to specify a voice channel to join!');
             }
+        } else {
+        // channel is explicitly specified, search for channel and join if possible
+            const voiceChannel = message.guild.channels.cache.find(vc => vc.name.toLowerCase() === args.join(' ').toLowerCase());
+            
+            if (voiceChannel) {
+                connection = await voiceChannel.join();
+                console.log(`Joined voice channel: ${voiceChannel.name}`);
+            } else {
+                message.reply('You need to specify a valid voice channel to join!');
+            }
+        }
 
-            const dispatcher = connection.play('assets/audio/chikenhig.mp3', { volume: 0.30 });
-            console.log('Playing chickenhig.mp3');            
+        // play audio file
+        const dispatcher = connection.play('assets/audio/chikenhig.mp3', { volume: 0.30 });
+        console.log('Playing chickenhig.mp3');            
 
-            dispatcher.on('finish', () => {
-                connection.disconnect();
-            });
-        })();
+        // disconnect on audio file finish
+        dispatcher.on('finish', () => {
+            connection.disconnect();
+            console.log(`Left voice channel: ${message.guild.me.voice.channel.name}`);
+        });
 	},
 };
