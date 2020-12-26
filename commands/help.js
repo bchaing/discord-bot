cfc914e423819573fa82c8acb5e6b1e47dda99cb
Brandon Chaing <bchaing@ucsd.edu>
2020-12-26 02:27:59 -0800

    Update comments for clarity

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
         // retrieve collection of commands
         const { commands } = message.client;
 
-
+        // display main help page
         if (!args.length) {
             const helpEmbed = new MessageEmbed()
                 .setColor('#eda338')
@@ -24,13 +24,16 @@ module.exports = {
             return;
         }
 
+        // search for command by inputted name
         const name = args[0].toLowerCase();
         const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
+        // check if command exists
         if (!command) {
             return message.channel.send(`${command} is not a valid command!`);
         }
 
+        // displays specific command help page
         const helpEmbed = new MessageEmbed()
             .setColor('#eda338')
             .setTitle(`${command.name}`)
@@ -38,11 +41,11 @@ module.exports = {
             .setDescription(`${command.description}`)
             .setTimestamp();
 
+        // displays options for specific commands
         if (command.description) helpEmbed.discription = `${command.description}`;
         if (command.aliases) helpEmbed.addField('Aliases', `\`${command.aliases.join(', ')}\``, true);
         if (command.usage) helpEmbed.addField('Usage', `\`b!${command.name} ${command.usage}\``, true);
-
-        if (command.cooldown) helpEmbed.addField('Cooldown', `\`${command.cooldown || 0}\` second(s)`, true);
+        if (command.cooldown) helpEmbed.addField('Cooldown', `\`${command.cooldown || 0} second(s)\``, true);
 
         message.channel.send(helpEmbed);
 	},
