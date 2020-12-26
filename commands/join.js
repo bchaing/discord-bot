af9f148849323aa8cd7f59a143b1cb798f09ba42
Brandon Chaing <bchaing@ucsd.edu>
2020-12-26 02:30:29 -0800

    Update comments for clarity

--- commands/join.js
+++ commands/join.js
@@ -5,19 +5,25 @@ module.exports = {
 	usage: '<voice channel>',
 	execute(message, args) {        
         if (args.length === 0) {
+        // check if user specified a voice channel
             if (message.member.voice.channel) {
+            // join user's voice channel if possible
                 message.member.voice.channel.join();
                 console.log(`Joined voice channel: ${message.member.voice.channel.name}`);
             } else {
+            // sends response if unable to join
                 message.reply('You need to specify a voice channel to join!');
             }
         } else {
+        // search for user specified channel and attempt to join
             const voiceChannel = message.guild.channels.cache.find(vc => vc.name.toLowerCase() === args.join(' ').toLowerCase());
             
             if (voiceChannel) {
+            // join voice channel if found
                 voiceChannel.join();
                 console.log(`Joined voice channel: ${voiceChannel.name}`);
             } else {
+            // notifies user that voice channel could not be found
                 message.reply('You need to specify a valid voice channel to join!');
             }
         }
