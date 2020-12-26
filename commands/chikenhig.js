dfe4502a72b4a6d4a8873e311d5e5b66cba0f177
Brandon Chaing <bchaing@ucsd.edu>
2020-12-25 22:42:50 -0800

    Update functionality to accept input channel

--- commands/chikenhig.js
+++ commands/chikenhig.js
@@ -3,23 +3,21 @@ module.exports = {
     description: 'Plays chikenhig insults.',
     guildOnly: true,
 	execute(message) {
-        (async () => {
-            // Only try to join the sender's voice channel if they are in one themselves
-            let connection;
-            if (message.member.voice.channel) {
-                connection = await message.member.voice.channel.join().catch(console.error);
-                console.log(`Joined voice channel: ${message.member.voice.channel.name}`);
-            } else {
-                message.reply('You are not connected to a valid voice channel!');
-                return;
-            }
+        // Only try to join the sender's voice channel if they are in one themselves
+        let connection;
+        if (message.member.voice.channel) {
+            connection = await message.member.voice.channel.join().catch(console.error);
+            console.log(`Joined voice channel: ${message.member.voice.channel.name}`);
+        } else {
+            message.reply('You are not connected to a valid voice channel!');
+            return;
+        }
 
-            const dispatcher = connection.play('assets/audio/chikenhig.mp3', { volume: 0.30 });
-            console.log('Playing chickenhig.mp3');            
+        const dispatcher = connection.play('assets/audio/chikenhig.mp3', { volume: 0.30 });
+        console.log('Playing chickenhig.mp3');            
 
-            dispatcher.on('finish', () => {
-                connection.disconnect();
-            });
-        })();
+        dispatcher.on('finish', () => {
+            connection.disconnect();
+        });
 	},
 };
--- commands/join.js
+++ commands/join.js
@@ -2,15 +2,23 @@ module.exports = {
 	name: 'join',
     description: 'Joins the calling member\'s voice channel.',
     guildOnly: true,
-	execute(message) {
-        (async () => {
-            // Only try to join the sender's voice channel if they are in one themselves
+	execute(message, args) {        
+        if (args.length === 0) {
             if (message.member.voice.channel) {
                 message.member.voice.channel.join();
                 console.log(`Joined voice channel: ${message.member.voice.channel.name}`);
             } else {
-                message.reply('You need to join a voice channel first!');
+                message.reply('You need to specify a voice channel to join!');
             }
-        })();
+        } else {
+            const voiceChannel = message.guild.channels.cache.find(vc => vc.name.toLowerCase() === args.join(' ').toLowerCase());
+            
+            if (voiceChannel) {
+                voiceChannel.join();
+                console.log(`Joined voice channel: ${voiceChannel.name}`);
+            } else {
+                message.reply('You need to specify a valid voice channel to join!');
+            }
+        }
 	},
 };
