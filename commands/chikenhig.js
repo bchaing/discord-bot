fd91dc6cbba66ba12839dce615952f0270a5b222
Brandon Chaing <bchaing@ucsd.edu>
2020-12-25 22:48:01 -0800

    Revert changes

--- commands/chikenhig.js
+++ commands/chikenhig.js
@@ -2,30 +2,24 @@ module.exports = {
 	name: 'chikenhig',
     description: 'Plays chikenhig insults.',
     guildOnly: true,
-	execute(message, args) {
-        if (args.length === 0) {
+	execute(message) {
+        (async () => {
+            // Only try to join the sender's voice channel if they are in one themselves
+            let connection;
             if (message.member.voice.channel) {
-                message.member.voice.channel.join();
+                connection = await message.member.voice.channel.join().catch(console.error);
                 console.log(`Joined voice channel: ${message.member.voice.channel.name}`);
             } else {
-                message.reply('You need to specify a voice channel to join!');
+                message.reply('You are not connected to a valid voice channel!');
+                return;
             }
-        } else {
-            const voiceChannel = message.guild.channels.cache.find(vc => vc.name.toLowerCase() === args.join(' ').toLowerCase());
-            
-            if (voiceChannel) {
-                voiceChannel.join();
-                console.log(`Joined voice channel: ${voiceChannel.name}`);
-            } else {
-                message.reply('You need to specify a valid voice channel to join!');
-            }
-        }
 
-        const dispatcher = connection.play('assets/audio/chikenhig.mp3', { volume: 0.30 });
-        console.log('Playing chickenhig.mp3');            
+            const dispatcher = connection.play('assets/audio/chikenhig.mp3', { volume: 0.30 });
+            console.log('Playing chickenhig.mp3');            
 
-        dispatcher.on('finish', () => {
-            connection.disconnect();
-        });
+            dispatcher.on('finish', () => {
+                connection.disconnect();
+            });
+        })();
 	},
 };
