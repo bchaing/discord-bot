c7a24f47d5a980a3a04b4e5dbd4f7b6cd3fb3dcc
Brandon Chaing <bchaing@ucsd.edu>
2021-01-06 22:35:24 -0800

    Update add cat command

---
+++ commands/general/randomcat.js
@@ -0,0 +1,21 @@
+const { Command } = require('discord.js-commando');
+const fetch = require('node-fetch');
+
+module.exports = class CatCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'randomcat',
+            aliases: ['meow', 'cat', 'purr'],
+            group: 'general',
+            memberName: 'randomcat',
+            description: 'Sends a random image of a dog.',
+        });
+    }
+
+    async run(message) {
+        const response = await fetch('https://api.thecatapi.com/v1/images/search');
+        const responseJSON = await response.json();
+
+        return message.say(responseJSON[0].url);
+    }
+};
