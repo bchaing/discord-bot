d53974b70c6398e9684215c94a55ed8148434bd1
Brandon Chaing <bchaing@ucsd.edu>
2021-02-12 19:19:34 -0800

    Added mocking spongebob text generator

---
+++ commands/fun/mocking.js
@@ -0,0 +1,36 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class MockCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'mock',
+            group: 'fun',
+            memberName: 'mock',
+            description: 'Adds mocking Spongebob capitalization to text.',
+            args: [
+                {
+                    key: 'text',
+                    type: 'string',
+                    prompt: 'What is the text that you want to use?',
+                    default: '',
+                },
+            ],
+        });
+    }
+
+    run(message, { text }) {
+        if (!text) return;
+
+        let retString = '';
+
+        for (let i = 0; i < text.length; i++) {
+            if (Math.random() < 0.5) {
+                retString += text.charAt(i).toUpperCase();
+            } else {
+                retString += text.charAt(i);
+            }
+        }
+
+        return message.say(retString);
+    }
+};
