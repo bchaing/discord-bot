77eb68487eda0c660ad393870f6da75f8b773549
bchaing <bchaing@ucsd.edu>
2021-03-04 01:33:31 -0800

    Set up files for raspberry pi

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [{
+    name: 'Bonk Bot',
+    script: 'index.js',
+    watch: '.',
+    ignore_watch: ["node_modules", "assets", "database.sqlite", "database.sqlite-journal"],
+    args: [
+	    "--color"
+        ]
+    }
+  ],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+}
