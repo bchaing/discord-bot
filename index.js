68bf5ef530e65dfc200b2b1977a8f04911647562
Brandon Chaing <bchaing@ucsd.edu>
2021-04-24 14:44:33 -0700

    Converted tabs to spaces for index.js file

--- index.js
+++ index.js
@@ -6,30 +6,30 @@ const { prefix, token, ownerID } = require('./config.json');
 
 // console timestamps
 require('console-stamp')(console, 
-	{ pattern: 'm/dd/yy HH:MM:ss', label: false, colors: { stamp: 'green' } });
+    { pattern: 'm/dd/yy HH:MM:ss', label: false, colors: { stamp: 'green' } });
 
 // create commando client
 const client = new CommandoClient({
-	commandPrefix: prefix,
+    commandPrefix: prefix,
     owner: ownerID,
 });
 
 // register the command groups, args types, and default comm
 client.registry
-	.registerDefaultTypes()
-	.registerGroups([
+    .registerDefaultTypes()
+    .registerGroups([
         ['currency', 'Currency'],
         ['fun', 'Fun'],
         ['general', 'General'],
         ['info', 'Info'],
         ['voice', 'Voice'],
-	])
-	.registerDefaultGroups()
-	.registerDefaultCommands({
-        help: false,
-        ping: false,
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands({
+         help: false,
+        ping: false,	
     })
-	.registerCommandsIn(path.join(__dirname, 'commands'));
+    .registerCommandsIn(path.join(__dirname, 'commands'));
 
 // prevent commands from executing for muted users
 client.dispatcher.addInhibitor(msg => {
@@ -38,15 +38,15 @@ client.dispatcher.addInhibitor(msg => {
   
 // event handler
 const eventFiles = 
-	fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+    fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args, client));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args, client));
-	}
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
 }
 
 client.login(token);
