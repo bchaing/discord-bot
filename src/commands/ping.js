47cb5da6cebe2b31b8a76b8fc339ee2aa869d56b
Brandon Chaing <bchaing@ucsd.edu>
2022-06-17 16:35:20 -0700

    Created ping command

---
+++ src/commands/ping.js
@@ -0,0 +1,27 @@
+const { Command } = require("@sapphire/framework");
+
+class PingCommand extends Command {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      name: "ping",
+      aliases: ["pong"],
+      description: "Display the latency of command replies.",
+    });
+  }
+
+  async messageRun(message) {
+    // send test message
+    const msg = await message.channel.send("🏓 Pong!");
+
+    // measure latency and edit message
+    const content = `🏓 Pong! \n Bot Latency: ${Math.round(
+      this.container.client.ws.ping,
+    )}ms\n API Latency: ${msg.createdTimestamp - message.createdTimestamp}ms`;
+    return msg.edit(content);
+  }
+}
+
+module.exports = {
+  PingCommand,
+};
