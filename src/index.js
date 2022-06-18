1187124825d0dbc7e9316eeafd3503fe7f447d64
Brandon Chaing <bchaing@ucsd.edu>
2022-06-17 20:34:17 -0700

    Added default prefix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const { SapphireClient } = require("@sapphire/framework");
 const dotenv = require("dotenv");
 
+// load env variables
+dotenv.config();
+
 // init client instance
 const client = new SapphireClient({
   intents: ["GUILDS", "GUILD_MESSAGES"],
   loadMessageCommandListeners: true,
+  defaultPrefix: process.env.DEFAULT_PREFIX,
   presence: {
     status: "online",
     activities: [{ type: "WATCHING", name: "for commands!" }],
   },
 });
-
-// load env variables
-dotenv.config();
 
 client.login(process.env.DISCORD_TOKEN).catch(console.error);
