6a919e5387b3c5b24328edd31a3a4341110360af
Brandon Chaing <bchaing@ucsd.edu>
2022-06-19 14:34:07 -0700

    Added avatar command

---
+++ src/commands/General/avatar.js
@@ -0,0 +1,35 @@
+const { Command } = require("@sapphire/framework");
+
+class AvatarCommand extends Command {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      aliases: ["av", "icon", "pfp"],
+      description: "Displays the profile picture of the specified user.",
+      fullCategory: ["General"],
+    });
+  }
+
+  async messageRun(message, args) {
+    // get members from message
+    const member = await args.pickResult("member");
+
+    // return if no members specified
+    if (!member.success) {
+      return message.reply(
+        "Please specify a valid user to get their profile picture.",
+      );
+    }
+
+    // get avatar url of member
+    const value = member.value;
+    const avatar = value.user.displayAvatarURL({ dynamic: true });
+
+    // send avatar url
+    message.channel.send(avatar).catch(this.container.logger.error);
+  }
+}
+
+module.exports = {
+  AvatarCommand,
+};
